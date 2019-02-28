6e57f961c53302ea1b6ebd6115d4bffe8c284189
Artem Ivanov <fast0490f@mail.ru>
2019-02-28 16:16:47 +0300

    test 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 const Plugin = require('./lib/plugin');
 const Rtsp = require('./lib/rtsp');
+const fs = require('fs');
 
 
 const plugin = new Plugin();
@@ -10,6 +11,29 @@ const STORE = {
   channels: { ws: {}, p2p: {} },
 };
 
+x = new Buffer('ffd8ffe000104a46494600010100000100010000ffdb0043000d090a0b0a080d0b0a0b0e0e0d0f13201513121213271c1e17202e2931302e292d2c333a4a3e333646372c2d405741464c4e525352323e5a615a50604a51524fffdb0043010e0e0e131113261515264f352d354f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4f4fffc00011080168028003012200021101031101ffc4001f0000010501010101010100000000000000000102030405060708090a0bffc400b5100002010303020403050504040000017d01020300041105122131410613516107227114328191a1082342b1c11552d1f02433627282090a161718191a25262728292a3435363738393a434445464748494a535455565758595a636465666768696a737475767778797a838485868788898a92939495969798999aa2a3a4a5a6a7a8a9aab2b3b4b5b6b7b8b9bac2c3c4c5c6c7c8c9cad2d3d4d5d6d7d8d9dae1e2e3e4e5e6e7e8e9eaf1f2f3f4f5f6f7f8f9faffc4001f0100030101010101010101010000000000000102030405060708090a0bffc400b51100020102040403040705040400010277000102031104052131061241510761711322328108144291a1b1c109233352f0156272d10a162434e125f11718191a262728292a35363738393a434445464748494a535455565758595a636465666768696a737475767778797a82838485868788898a92939495969798999aa2a3a4a5a6a7a8a9aab2b3b4b5b6b7b8b9bac2c3c4c5c6c7c8c9cad2d3d4d5d6d7d8d9dae2e3e4e5e6e7e8e9eaf2f3f4f5f6f7f8f9faffda000c03010002110311003f00','hex')
+let buf = [x]
+
+function rtsp_jpeg({ id, data }) {
+
+  if (data[0] === 160) {
+    const l = data[data.length - 1];
+    buf.push(data.slice(20, data.length - l))
+  } else {
+    buf.push(data.slice(20))
+  }
+
+  if (data[1] === 154) {
+    x = Buffer.concat(buf);
+    fs.writeFileSync('t0.jpg',x)
+    buf = [x];
+    process.exit(0)
+  }
+
+
+  // console.log(data.slice(12))
+
+}
 
 function rtsp_stream({ id, data }) {
   if (STORE.cams[id] !== undefined) {
@@ -37,6 +61,7 @@ function create_rtsp(id, config) {
   STORE.cams[config.id].rtsp = new Rtsp(config);
   STORE.cams[config.id].rtsp.on('play', rtsp_play);
   STORE.cams[config.id].rtsp.on('stream', rtsp_stream);
+  STORE.cams[config.id].rtsp.on('jpeg', rtsp_jpeg);
 }
 
 function registrationchannel(socket, type, channelid) {
@@ -73,6 +98,8 @@ function wsmessage(ws, data) {
 
 function wsconnection(ws) {
   ws.on('message', data => wsmessage(ws, data));
+  ws.on('close', () => console.log('close', new Date().toString()));
+  ws.on('error', () => console.log('error', new Date().toString()));
 }
 
 function channel_settings(id, data) {
